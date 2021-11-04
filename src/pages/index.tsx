93252d9293271129ff13a2131cdd6023db55a9f4
DESKTOP-27DRGO2\Dominik <s.domnik1321@gmail.com>
2021-11-04 14:15:34 +0100

    finished site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -238,13 +238,7 @@ const IndexPage: React.FC = (): JSX.Element => {
 
   const handleFormAppears = () =>{
     const tl = gsap.timeline()
-    let width:number;
-    if(window.innerWidth <= 1024){
-      width == 95
-    }else{
-      width = 60
-    }
-    tl.to('.home__form',{width:`${width}%`,border:'2px solid white', force3D:true,duration:1})
+    tl.to('.home__form',{width:window.innerWidth <= 1024 ? "95%" : '60%',border:'2px solid white', force3D:true,duration:1})
       .to('.home__form',{height:'650px', force3D:true,duration:1})
   }
   const handleFormDisappears = () =>{
